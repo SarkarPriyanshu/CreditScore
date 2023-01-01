3ccf1b593394cbe2e697e14ec0bfcdcfb3b2da32
SarkarPriyanshu(Poizn) <55652493+SarkarPriyanshu@users.noreply.github.com>
2023-01-01 00:34:48 -0800

    Update server.py

--- server.py
+++ server.py
@@ -7,8 +7,10 @@ import utils.cleaner as cleaner
 app = Flask(__name__)
 CORS(app)
 
+# Loading the model of XGboostClassifier.
 XGBoostClassifier_model = pickle.load(open('Model/XGBoostClassifierPipeline.pkl','rb'))
 
+# Get Info route.
 @app.route('/',methods=['GET'])
 def home():
     return {
@@ -19,7 +21,7 @@ def home():
                     'content':"""Problem Statement You are working as a data scientist in a global finance company. Over the years, the company has collected basic bank details and gathered a lot of credit-related information. The management wants to build an intelligent system to segregate the people into credit score brackets to reduce the manual efforts.""",
                     }            
         }
-  
+# Predict random predictions from dataset.
 @app.route('/predict_auto',methods=['POST'])
 @cross_origin()
 def predict_auto():
@@ -29,7 +31,7 @@ def predict_auto():
     result = XGBoostClassifier_model.predict(data)
     return {'result':str(result[0])}
 
-
+# Predict user value to credict score.
 @app.route('/predict',methods=['POST'])
 @cross_origin()
 def predict():
